f3620acb168db10672c74719bef52e7384bb1c68
vignesh5142 <vigneshm5142@gmail.com>
2023-10-31 21:53:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-3">
+            <div className="card text-start m-lg-5 my-4">
+              <div className="date-box">
+                <p className="card-text-3d-text">Neon</p>
+                <p className="card-text-3d">3D</p>
+              </div>
+              <div className="content_box">
+                <h2 className="card-title text-light fw-bold">Card title</h2>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text-seeMore fw-bold">See More</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-3 card2">
+            <div className="card text-start m-lg-5 my-4">
+              <div className="date-box">
+                <p className="card-text-3d-text">Neon</p>
+                <p className="card-text-3d">3D</p>
+              </div>
+              <div className="content_box">
+                <h2 className="card-title text-light fw-bold">Card title</h2>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text-seeMore fw-bold">See More</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-3 card3">
+            <div className="card text-start m-lg-5 my-4">
+              <div className="date-box">
+                <p className="card-text-3d-text">Neon</p>
+                <p className="card-text-3d">3D</p>
+              </div>
+              <div className="content_box">
+                <h2 className="card-title text-light fw-bold">Card title</h2>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text-seeMore fw-bold">See More</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-3 card4">
+            <div className="card text-start m-lg-5 my-4">
+              <div className="date-box">
+                <p className="card-text-3d-text">Neon</p>
+                <p className="card-text-3d">3D</p>
+              </div>
+              <div className="content_box">
+                <h2 className="card-title text-light fw-bold">Card title</h2>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text-seeMore fw-bold">See More</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
